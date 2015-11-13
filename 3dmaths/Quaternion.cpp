3ff28db9b469f555675bf67887d0f3885e1c816e
ljcduo <501297819@qq.com>
2015-11-13 09:03:57 +0800

    修改四元数注释为中文。添加.gitignore。

--- 3dmaths/Quaternion.cpp
+++ 3dmaths/Quaternion.cpp
@@ -1,11 +1,15 @@
 ////////////////////////////////////////////////////////////////////////////
 //
+// 3D数学基础基础：游戏与图形开发
 // 3D Math Primer for Games and Graphics Development
 //
+// Quaternion.cpp - 四元数实现
 // Quaternion.cpp - Quaternion implementation
 //
+// 登录gamemath.com以获得该文件的最新版本
 // Visit gamemath.com for the latest version of this file.
 //
+// 更多细节参见11.3节
 // For more details see section 11.3.
 //
 /////////////////////////////////////////////////////////////////////////////
@@ -20,10 +24,12 @@
 
 /////////////////////////////////////////////////////////////////////////////
 //
+// 全局数据
 // global data
 //
 /////////////////////////////////////////////////////////////////////////////
 
+// 全局标识符四元数。注意这里没有四元数类的构造函数，因为我们不需要。
 // The global identity quaternion.  Notice that there are no constructors
 // to the Quaternion class, since we really don't need any.
 
@@ -33,6 +39,7 @@ const Quaternion kQuaternionIdentity = {
 
 /////////////////////////////////////////////////////////////////////////////
 //
+// 四元数成员类
 // class Quaternion members
 //
 /////////////////////////////////////////////////////////////////////////////
@@ -43,14 +50,17 @@ const Quaternion kQuaternionIdentity = {
 // Quaternion::setToRotateAboutZ
 // Quaternion::setToRotateAboutAxis
 //
+// 构建四元数绕指定轴旋转
 // Setup the quaternion to rotate about the specified axis
 
 void	Quaternion::setToRotateAboutX(float theta) {
 
+	// 计算半角
 	// Compute the half angle
 
 	float	thetaOver2 = theta * .5f;
 
+	// 设置值
 	// Set the values
 
 	w = cos(thetaOver2);
@@ -61,10 +71,12 @@ void	Quaternion::setToRotateAboutX(float theta) {
 
 void	Quaternion::setToRotateAboutY(float theta) {
 
+	// 计算半角
 	// Compute the half angle
 
 	float	thetaOver2 = theta * .5f;
 
+	// 设置值
 	// Set the values
 
 	w = cos(thetaOver2);
@@ -75,10 +87,12 @@ void	Quaternion::setToRotateAboutY(float theta) {
 
 void	Quaternion::setToRotateAboutZ(float theta) {
 
+	// 计算半角
 	// Compute the half angle
 
 	float	thetaOver2 = theta * .5f;
 
+	// 设置值
 	// Set the values
 
 	w = cos(thetaOver2);
@@ -89,15 +103,18 @@ void	Quaternion::setToRotateAboutZ(float theta) {
 
 void	Quaternion::setToRotateAboutAxis(const Vector3 &axis, float theta) {
 
+	// 这个旋转轴必须已单位化
 	// The axis of rotation must be normalized
 
 	assert(fabs(vectorMag(axis) - 1.0f) < .01f);
 
+	// 计算半角和它的sin值
 	// Compute the half angle and its sin
 
 	float	thetaOver2 = theta * .5f;
 	float	sinThetaOver2 = sin(thetaOver2);
 
+	// 设置值
 	// Set the values
 
 	w = cos(thetaOver2);
@@ -109,13 +126,16 @@ void	Quaternion::setToRotateAboutAxis(const Vector3 &axis, float theta) {
 //---------------------------------------------------------------------------
 // EulerAngles::setToRotateObjectToInertial
 //
+// 给定欧拉角形式的方位，设置四元数执行物体->惯性旋转
 // Setup the quaternion to perform an object->inertial rotation, given the
 // orientation in Euler angle format
 //
+// 参见10.6.5获得更多信息。
 // See 10.6.5 for more information.
 
 void	Quaternion::setToRotateObjectToInertial(const EulerAngles &orientation) {
 
+	// 计算半角的sin和cos值
 	// Compute sine and cosine of the half angles
 
 	float	sp, sb, sh;
@@ -124,6 +144,7 @@ void	Quaternion::setToRotateObjectToInertial(const EulerAngles &orientation) {
 	sinCos(&sb, &cb, orientation.bank * 0.5f);
 	sinCos(&sh, &ch, orientation.heading * 0.5f);
 
+	// 计算值
 	// Compute values
 
 	w =  ch*cp*cb + sh*sp*sb;
@@ -135,13 +156,16 @@ void	Quaternion::setToRotateObjectToInertial(const EulerAngles &orientation) {
 //---------------------------------------------------------------------------
 // EulerAngles::setToRotateInertialToObject
 //
+// 给定欧拉角形式的方位，构建四元数来执行惯性->物体旋转
 // Setup the quaternion to perform an object->inertial rotation, given the
 // orientation in Euler angle format
 //
+// 参见10.6.5获得更多信息。
 // See 10.6.5 for more information.
 
 void	Quaternion::setToRotateInertialToObject(const EulerAngles &orientation) {
 
+	// 计算半角的sin和cos值
 	// Compute sine and cosine of the half angles
 
 	float	sp, sb, sh;
@@ -150,6 +174,7 @@ void	Quaternion::setToRotateInertialToObject(const EulerAngles &orientation) {
 	sinCos(&sb, &cb, orientation.bank * 0.5f);
 	sinCos(&sh, &ch, orientation.heading * 0.5f);
 
+	// 计算值
 	// Compute values
 
 	w =  ch*cp*cb + sh*sp*sb;
@@ -161,6 +186,9 @@ void	Quaternion::setToRotateInertialToObject(const EulerAngles &orientation) {
 //---------------------------------------------------------------------------
 // Quaternion::operator *
 //
+// 四元数叉乘，用于连接相乘的欧拉角位移。相乘的顺序是从左到右，
+// 对应欧拉角位移的顺序。这是标准定义的不同之处。
+// 参见10.4.8来获得隐藏在背离标准定义的基本原理。
 // Quaternion cross product, which concatonates multiple angular
 // displacements.  The order of multiplication, from left to right,
 // corresponds to the order that the angular displacements are
@@ -182,14 +210,17 @@ Quaternion Quaternion::operator *(const Quaternion &a) const {
 //---------------------------------------------------------------------------
 // Quaternion::operator *=
 //
+// 合并叉乘和赋值，像C++约定习俗一样
 // Combined cross product and assignment, as per C++ convention
 
 Quaternion &Quaternion::operator *=(const Quaternion &a) {
 
+	// 相乘和赋值
 	// Multuiply and assign
 
 	*this = *this * a;
 
+	// 返回左值引用
 	// Return reference to l-value
 
 	return *this;
@@ -198,24 +229,30 @@ Quaternion &Quaternion::operator *=(const Quaternion &a) {
 //---------------------------------------------------------------------------
 // Quaternion::normalize
 //
+// “单位化”一个四元数。注意正常地，四元数都是单位化的（包含数值精确限制）。
+// 参见10.4.6来获得更多信息。
 // "Normalize" a quaternion.  Note that normally, quaternions
 // are always normalized (within limits of numerical precision).
 // See section 10.4.6 for more information.
 //
+// 这个提供的函数首先要解决浮点“错误蠕动”，这个想象可能会发生在许多四元数操作。
 // This function is provided primarily to combat floating point "error
 // creep," which can occur when many successive quaternion operations
 // are applied.
 
 void	Quaternion::normalize() {
 
+	// 计算四元数的大小
 	// Compute magnitude of the quaternion
 
 	float	mag = (float)sqrt(w*w + x*x + y*y + z*z);
 
+	// 检查非法长度，防止除0操作
 	// Check for bogus length, to protect against divide by zero
 
 	if (mag > 0.0f) {
 
+		// 单位化它
 		// Normalize it
 
 		float	oneOverMag = 1.0f / mag;
@@ -226,10 +263,12 @@ void	Quaternion::normalize() {
 
 	} else {
 
+		// 我们有一个问题
 		// Houston, we have a problem
 
 		assert(false);
 
+		// 在release模式，仅仅返回某些东西
 		// In a release build, just slam it to something
 
 		identity();
@@ -239,14 +278,17 @@ void	Quaternion::normalize() {
 //---------------------------------------------------------------------------
 // Quaternion::getRotationAngle
 //
+// 返回theta角旋转
 // Return the rotation angle theta
 
 float	Quaternion::getRotationAngle() const {
 
+	// 计算半角。 记住 w = cos(theta/2)
 	// Compute the half angle.  Remember that w = cos(theta / 2)
 
 	float thetaOver2 = safeAcos(w);
 
+	// 返回旋转角
 	// Return the rotation angle
 
 	return thetaOver2 * 2.0f;
@@ -254,30 +296,36 @@ float	Quaternion::getRotationAngle() const {
 
 //---------------------------------------------------------------------------
 // Quaternion::getRotationAxis
-//
+// 
+// 返回旋转轴
 // Return the rotation axis
 
 Vector3	Quaternion::getRotationAxis() const {
 
+	// 计算sin^2(theta/2)。记住w=cos(theta/2)和sin^2(x)+cos^2(x)=1
 	// Compute sin^2(theta/2).  Remember that w = cos(theta/2),
 	// and sin^2(x) + cos^2(x) = 1
 
 	float sinThetaOver2Sq = 1.0f - w*w;
 
+	// 防止数值不精确
 	// Protect against numerical imprecision
 
 	if (sinThetaOver2Sq <= 0.0f) {
 
+		// 单位四元数，或者数值不精确。仅仅返回任意可用的向量，因为它并不重要
 		// Identity quaternion, or numerical imprecision.  Just
 		// return any valid vector, since it doesn't matter
 
 		return Vector3(1.0f, 0.0f, 0.0f);
 	}
 
+	// 计算 1 / sin(theta/2)
 	// Compute 1 / sin(theta/2)
 
 	float	oneOverSinThetaOver2 = 1.0f / sqrt(sinThetaOver2Sq);
 
+	// 返回旋转轴
 	// Return axis of rotation
 
 	return Vector3(
@@ -289,6 +337,7 @@ Vector3	Quaternion::getRotationAxis() const {
 
 /////////////////////////////////////////////////////////////////////////////
 //
+// 非成员函数
 // Nonmember functions
 //
 /////////////////////////////////////////////////////////////////////////////
@@ -296,6 +345,7 @@ Vector3	Quaternion::getRotationAxis() const {
 //---------------------------------------------------------------------------
 // dotProduct
 //
+// 四元数点乘。我们使用非成员函数，因此传递四元数表达式不用稀奇古怪的语法
 // Quaternion dot product.  We use a nonmember function so we can
 // pass quaternion expressions as operands without having "funky syntax"
 //
@@ -308,21 +358,26 @@ float dotProduct(const Quaternion &a, const Quaternion &b) {
 //---------------------------------------------------------------------------
 // slerp
 //
+// 球面线性差值
 // Spherical linear interpolation.
 //
 // See 10.4.13
 
 Quaternion slerp(const Quaternion &q0, const Quaternion &q1, float t) {
 
+	// 检查超出范围参数，并且返回边界点
 	// Check for out-of range parameter and return edge points if so
 
 	if (t <= 0.0f) return q0;
 	if (t >= 1.0f) return q1;
 
+	// 使用点乘计算任意四元数之间的角度的cos值
 	// Compute "cosine of angle between quaternions" using dot product
 
 	float cosOmega = dotProduct(q0, q1);
 
+	// 如果负数点乘，使用-q1。两个四元数q和-q代表相同的旋转，
+	// 不过可能产生不同的球面差值。我们使用q或-q旋转精确的角度。
 	// If negative dot, use -q1.  Two quaternions q and -q
 	// represent the same rotation, but may produce
 	// different slerp.  We chose q or -q to rotate using
@@ -340,16 +395,19 @@ Quaternion slerp(const Quaternion &q0, const Quaternion &q1, float t) {
 		cosOmega = -cosOmega;
 	}
 
+	// 我们应该有两个单位四元数，所以点乘应该是<=0.0
 	// We should have two unit quaternions, so dot should be <= 1.0
 
 	assert(cosOmega < 1.1f);
 
+	// 计算插补分数，检查四元数差不多是相同的
 	// Compute interpolation fraction, checking for quaternions
 	// almost exactly the same
 
 	float k0, k1;
 	if (cosOmega > 0.9999f) {
 
+		// 非常接近 - 仅仅使用线性差值，我们防止除0操作
 		// Very close - just use linear interpolation,
 		// which will protect againt a divide by zero
 
@@ -358,26 +416,31 @@ Quaternion slerp(const Quaternion &q0, const Quaternion &q1, float t) {
 
 	} else {
 
+		// 使用三角等式sin^2(omega) + cos^2(omega) = 1计算角度的sin值
 		// Compute the sin of the angle using the
 		// trig identity sin^2(omega) + cos^2(omega) = 1
 
 		float sinOmega = sqrt(1.0f - cosOmega*cosOmega);
 
+		// 从sin和cos计算角度
 		// Compute the angle from its sin and cosine
 
 		float omega = atan2(sinOmega, cosOmega);
 
+		// 计算分母的倒数，所以我们只用做一次除法操作
 		// Compute inverse of denominator, so we only have
 		// to divide once
 
 		float oneOverSinOmega = 1.0f / sinOmega;
 
+		// 计算差值参数
 		// Compute interpolation parameters
 
 		k0 = sin((1.0f - t) * omega) * oneOverSinOmega;
 		k1 = sin(t * omega) * oneOverSinOmega;
 	}
 
+	// 插入
 	// Interpolate
 
 	Quaternion result;
@@ -386,6 +449,7 @@ Quaternion slerp(const Quaternion &q0, const Quaternion &q1, float t) {
 	result.z = k0*q0.z + k1*q1z;
 	result.w = k0*q0.w + k1*q1w;
 
+	// 返回它
 	// Return it
 
 	return result;
@@ -394,22 +458,26 @@ Quaternion slerp(const Quaternion &q0, const Quaternion &q1, float t) {
 //---------------------------------------------------------------------------
 // conjugate
 //
+// 计算四元数的共轭。这是原始四元数相反旋转的四元数。参见10.4.7
 // Compute the quaternion conjugate.  This is the quaternian
 // with the opposite rotation as the original quaternian.  See 10.4.7
 
 Quaternion conjugate(const Quaternion &q) {
 	Quaternion result;
 
+	// 相同旋转数
 	// Same rotation amount
 
 	result.w = q.w;
 
+	// 相反轴向
 	// Opposite axis of rotation
 
 	result.x = -q.x;
 	result.y = -q.y;
 	result.z = -q.z;
 
+	// 返回它
 	// Return it
 
 	return result;
@@ -418,12 +486,16 @@ Quaternion conjugate(const Quaternion &q) {
 //---------------------------------------------------------------------------
 // pow
 //
+// 四元数的幂
 // Quaternion exponentiation.
 //
+// 参见10.4.12
 // See 10.4.12
 
 Quaternion pow(const Quaternion &q, float exponent) {
 
+	// 检查标准四元数的情况
+	// 防止它进行除0操作
 	// Check for the case of an identity quaternion.
 	// This will protect against divide by zero
 
@@ -431,19 +503,23 @@ Quaternion pow(const Quaternion &q, float exponent) {
 		return q;
 	}
 
+	// 提取半角alpha(alpha=theta/2)
 	// Extract the half angle alpha (alpha = theta/2)
 
 	float	alpha = acos(q.w);
 
+	// 计算新alpha值
 	// Compute new alpha value
 
 	float	newAlpha = alpha * exponent;
 
+	// 计算新w值
 	// Compute new w value
 
 	Quaternion result;
 	result.w = cos(newAlpha);
 
+	// 计算新xyz值
 	// Compute new xyz values
 
 	float	mult = sin(newAlpha) / sin(alpha);
@@ -451,6 +527,7 @@ Quaternion pow(const Quaternion &q, float exponent) {
 	result.y = q.y * mult;
 	result.z = q.z * mult;
 
+	// 返回它
 	// Return it
 
 	return result;
--- 3dmaths/Quaternion.h
+++ 3dmaths/Quaternion.h
@@ -1,11 +1,15 @@
 /////////////////////////////////////////////////////////////////////////////
 //
+// 3D数学基础：游戏和图形开发
 // 3D Math Primer for Games and Graphics Development
 //
+// Quaternion.h - 四元数类声明
 // Quaternion.h - Declarations for class Quaternion
 //
+// 访问gamemath.com来获得该文件的最新版本
 // Visit gamemath.com for the latest version of this file.
 //
+// 更多信息，参见Quaternion.cpp
 // For more details, see Quaternion.cpp
 //
 /////////////////////////////////////////////////////////////////////////////
@@ -17,16 +21,22 @@ class Vector3;
 class EulerAngles;
 
 //---------------------------------------------------------------------------
+// 四元数类
 // class Quaternion
 //
+// 实现四元数，呈现3D空间中的角位移（方位）
 // Implement a quaternion, for purposes of representing an angular
 // displacement (orientation) in 3D.
 
 class Quaternion {
 public:
 
+// 公有数据
 // Public data
 
+	// 四元数的这4个值。通常地，不会直接地操作它们。
+	// 然而，我们让它设为公有，因为禁止直接进入会使一些操作更复杂，
+	// 诸如文件I/O操作。
 	// The 4 values of the quaternion.  Normally, it will not
 	// be necessary to manipulate these directly.  However,
 	// we leave them public, since prohibiting direct access
@@ -35,12 +45,15 @@ public:
 
 	float	w, x, y, z;
 
+// 公有操作
 // Public operations
 
+	// 设置为单位数
 	// Set to identity
 
 	void	identity() { w = 1.0f; x = y = z = 0.0f; }
 
+	// 构建指定轴旋转的四元数
 	// Setup the quaternion to a specific rotation
 
 	void	setToRotateAboutX(float theta);
@@ -48,46 +61,56 @@ public:
 	void	setToRotateAboutZ(float theta);
 	void	setToRotateAboutAxis(const Vector3 &axis, float theta);
 
+	// 给定一个欧拉角形式的方位，构建执行物体<->惯性旋转
 	// Setup to perform object<->inertial rotations,
 	// given orientation in Euler angle format
 
 	void	setToRotateObjectToInertial(const EulerAngles &orientation);
 	void	setToRotateInertialToObject(const EulerAngles &orientation);
 
+	// 叉乘
 	// Cross product
 
 	Quaternion operator *(const Quaternion &a) const;
 
+	// 相乘和赋值结合，遵循C++约定习俗
 	// Multiplication with assignment, as per C++ convention
 
 	Quaternion &operator *=(const Quaternion &a);
 
+	// 单位化四元数。
 	// Normalize the quaternion.
 
 	void	normalize();
 
+	// 提取和返回旋转角度和旋转轴。
 	// Extract and return the rotation angle and axis.
 
 	float	getRotationAngle() const;
 	Vector3	getRotationAxis() const;
 };
 
+// 一个全局唯一标识符四元数常量
 // A global "identity" quaternion constant
 
 extern const Quaternion kQuaternionIdentity;
 
+// 四元数点乘
 // Quaternion dot product.
 
 extern float dotProduct(const Quaternion &a, const Quaternion &b);
 
+// 球面线性插值
 // Spherical linear interpolation
 
 extern Quaternion slerp(const Quaternion &p, const Quaternion &q, float t);
 
+// 四元数共轭
 // Quaternion conjugation
 
 extern Quaternion conjugate(const Quaternion &q);
 
+// 四元数的幂
 // Quaternion exponentiation
 
 extern Quaternion pow(const Quaternion &q, float exponent);
